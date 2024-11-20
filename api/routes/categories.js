53f79c812a9474f43a57bca339f2c0c441e75d29
mehmetdurankaya <mehmetdurankaya@gmai.com>
2024-11-20 14:17:18 +0300

    #13 Excell dışarı aktarım (Export) tamamlandı.

--- api/routes/categories.js
+++ api/routes/categories.js
@@ -10,8 +10,8 @@ const logger=require("../lib/logger/LoggerClass");
 const auth= require("../lib/auth")();
 const i18n=new(require("../lib/i18n"))(config.DEFAULT_LANG);
 const emitter = require("../lib/Emitter");
-
-
+const excelExport = new (require("../lib/Export"))();
+const fs = require("fs");
 
 router.all("*",auth.authenticate(),(req,res,next)=>{
     next();
@@ -82,5 +82,28 @@ router.delete("/delete",auth.checkRoles("category_delete"),async(req,res)=>{
     res.status(errorResponse.code).json(errorResponse);
   }
 });
+router.post("/export", auth.checkRoles("category_export"), async (req, res) => {
+  try {
+      let categories = await Categories.find({});
 
+
+      let excel = excelExport.toExcel(
+          ["NAME", "IS ACTIVE?", "USER_ID", "CREATED AT", "UPDATED AT"],
+          ["name", "is_active", "created_by", "created_at", "updated_at"],
+          categories
+      )
+
+      let filePath = __dirname + "/../tmp/categories_excel_" + Date.now() + ".xlsx";
+
+      fs.writeFileSync(filePath, excel, "UTF-8");
+
+      res.download(filePath);
+
+      // fs.unlinkSync(filePath);
+
+  } catch (err) {
+      let errorResponse = Response.errorResponse(err);
+      res.status(errorResponse.code).json(Response.errorResponse(err));
+  }
+});
 module.exports = router;
